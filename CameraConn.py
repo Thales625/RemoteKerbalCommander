30bdc8e8538caab319a03469e5b87155266166c7
Thales Rodrigues <thaleshend@gmail.com>
2024-02-14 16:08:13 -0300

    fix: camera conn

--- CameraConn.py
+++ CameraConn.py
@@ -2,18 +2,40 @@ import grpc
 import cam_pb2
 from cam_pb2_grpc import CameraStreamStub
 
-from Camera import Camera
-
-ADDRESS = 'localhost'
-PORT = 5077
+from PIL import Image
+from io import BytesIO
 
 def debug(text:str):
     print(f"CameraConn > {text}")
 
+ADDRESS = 'localhost'
+PORT = 5077
+
+WIDTH = 180
+HEIGHT = 180
+QUANTIZE = 32
+
+class Camera:
+    def __init__(self, id, stub, text_req) -> None:
+        self.id = id
+        self.text_req = text_req(cameraId=self.id)
+        self.stub = stub
+
+    def get_image(self):
+        img_byte = self.stub(self.text_req).texture
+        image = Image.open(BytesIO(img_byte))
+        image = image.resize((WIDTH, HEIGHT), Image.NEAREST)
+        image = image.quantize(QUANTIZE)
+
+        image_bytes = BytesIO()
+        image.save(image_bytes, format="png")
+
+        return image_bytes.getvalue()
+
 class CameraConnection:
     def __init__(self) -> None:
         self.on_error = lambda reason: None
-        
+
         self.cameras = []
 
         self.channel = grpc.insecure_channel(f"{ADDRESS}:{PORT}")
